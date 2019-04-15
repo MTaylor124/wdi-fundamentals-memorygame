f3d95c010bbf90b9178da8d75ba0aa912e43fd87
matt <okokok.com>
2019-04-15 13:42:02 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+var cardOne = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+cardsInPlay.push(cards[0]);
+//console.log(cardsInPlay);
+
+
+var cardTwo = "queen";
+cardsInPlay.push(cards[1]);
+console.log("user flipped " + cardTwo);
+console.log(cardsInPlay);
+
+if ( cardsInPlay[0] === cardsInPlay[1] ) {
+	alert("congrats you have a match"); 
+}
+else {
+	alert("these cards don't go together! what are you, blind?");
+}
